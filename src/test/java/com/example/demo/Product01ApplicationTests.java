31578b4251a1c8334a34792d54b37c3e0928f148
穂積高之 <tikitaka@MacBook-Pro.local>
2023-03-28 11:36:59 +0900

    little fix for default test code

--- src/test/java/com/example/demo/Product01ApplicationTests.java
+++ src/test/java/com/example/demo/Product01ApplicationTests.java
@@ -1,9 +1,11 @@
 package com.example.demo;
 
 import org.junit.jupiter.api.Test;
+import org.springframework.boot.SpringBootConfiguration;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
+@SpringBootConfiguration
 class Product01ApplicationTests {
 
 	@Test
